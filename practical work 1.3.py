a56bf2857a033255ff2c6e271a2c40463e849859
Zkera <108960099+Zkera@users.noreply.github.com>
2024-09-11 08:58:37 +0300

    Update practical work 1.3.py

--- practical work 1.3.py
+++ practical work 1.3.py
@@ -1,5 +1,14 @@
 d = dict()
+d["a"] = ["1"]
 for i in range(int(input())):
     str = input().split()
-    #if not (str[0] in d):
-        #d.
+    if not (str[0] in d):
+    	d[str[0]] = list()
+    d[str[0]].append(str[2])
+print(d)
+max = "a"
+for key in d.keys():
+	if len(d[key])> len(d[max]):
+		max = key
+print("\n\nanswer -", max)
+#admin 11.09.2024 193.168.0.1
